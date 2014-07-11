51a096cbde3d3c714f6426c1da3d5638368b3d41
Stanley Ng <stanleyhlng@yahoo.com.hk>
2014-07-10 20:43:22 -0700

    Initial Commit

---
+++ ios_twitter_redux/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ios_twitter_redux
+//
+//  Created by Stanley Ng on 7/10/14.
+//  Copyright (c) 2014 Stanley Ng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ios_twitter_reduxTests/ios_twitter_reduxTests.m
@@ -0,0 +1,34 @@
+//
+//  ios_twitter_reduxTests.m
+//  ios_twitter_reduxTests
+//
+//  Created by Stanley Ng on 7/10/14.
+//  Copyright (c) 2014 Stanley Ng. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ios_twitter_reduxTests : XCTestCase
+
+@end
+
+@implementation ios_twitter_reduxTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
