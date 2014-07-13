c7fd50d44fe13a1d7d00b2b0ba9add5612595cd5
Stanley Ng <stanleyhlng@yahoo.com.hk>
2014-07-13 11:22:57 -0700

    update mentions timeline

---
+++ ios_twitter_redux/TwitterClient.h
@@ -0,0 +1,13 @@
+//
+//  TwitterClient.h
+//  ios_twitter_redux
+//
+//  Created by Stanley Ng on 7/13/14.
+//  Copyright (c) 2014 Stanley Ng. All rights reserved.
+//
+
+#import "BDBOAuth1RequestOperationManager.h"
+
+@interface TwitterClient : BDBOAuth1RequestOperationManager
+
+@end
---
+++ ios_twitter_redux/TwitterClient.m
@@ -0,0 +1,13 @@
+//
+//  TwitterClient.m
+//  ios_twitter_redux
+//
+//  Created by Stanley Ng on 7/13/14.
+//  Copyright (c) 2014 Stanley Ng. All rights reserved.
+//
+
+#import "TwitterClient.h"
+
+@implementation TwitterClient
+
+@end
