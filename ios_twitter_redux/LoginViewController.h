ad1f976f740343c3b840df9b82626ffc1e695462
Stanley Ng <stanleyhlng@yahoo.com.hk>
2014-07-13 11:08:35 -0700

    create login view

---
+++ ios_twitter_redux/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  ios_twitter_redux
+//
+//  Created by Stanley Ng on 7/13/14.
+//  Copyright (c) 2014 Stanley Ng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
